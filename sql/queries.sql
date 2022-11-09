8d013562e52ccb5baa92e0d2c185b72d837fcd37
edkrueger <edkrueger@gmail.com>
2022-11-09 16:51:37 -0600

    Fix the sample query.

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,7 +1,11 @@
 select
+	business_info -> 'id' as id,
 	business_info -> 'name' as name,
 	business_info -> 'alias' as alias,
 	business_info -> 'categories' -> 0 -> 'alias'  as category_0,
 	business_info -> 'categories' -> 1 -> 'alias'  as category_1,
 	business_info -> 'categories' -> 2 -> 'alias'  as category_2,
-	business_info -> 'categories' -> 3 -> 'alias'  as category_3
+	business_info -> 'categories' -> 3 -> 'alias'  as category_3,
+	business_info
+from
+	business
